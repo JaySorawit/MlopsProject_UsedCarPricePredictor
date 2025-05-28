79c8ddf91936a19e544107d9dd87d28471b2ed69
Woraphol Sae ku <woraphol69@hotmail.com>
2025-05-28 15:07:11 +0700

    Update preprocess_data.py

--- airflow/src/preprocess_data.py
+++ airflow/src/preprocess_data.py
@@ -2,17 +2,19 @@ import pandas as pd
 import os
 
 def preprocess_data():
+
     input_name = 'cleaned_data.csv'
     # output_path = '/opt/airflow/data/processed.csv'
     DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/"))
     input_path = os.path.join(DIR, input_name)
     df = pd.read_csv(input_path)
-    
+
     # ตัวอย่าง: ลบคอลัมน์ที่ไม่จำเป็น
+    # # ตัวอย่าง: ลบคอลัมน์ที่ไม่จำเป็น
     df = df.dropna()
-    df = df.select_dtypes(include=['number'])  # เอาเฉพาะข้อมูลตัวเลข
-    df.to_csv(output_path, index=False)
+    print(df)
+    # df = df.select_dtypes(include=['number'])  # เอาเฉพาะข้อมูลตัวเลข
+    # df.to_csv(output_path, index=False)
 
     # print(f"Preprocessed data saved to {output_path}")
-
 preprocess_data()
